1aad42edea0344c4369ee67e4ca17f7806081951
Davi Albino <negroalbino_@hotmail.com>
2023-05-15 22:18:29 -0300

    feat: implement text importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):    
+
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido")
+
+    try:
+        with open(path_file) as pf:
+            rows = pf.read().split("\n")
+            return rows
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
