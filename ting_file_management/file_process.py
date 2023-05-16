0c27c0eeaa6e0c1a3d89aa6652fadd3c479a2dca
Davi Albino <negroalbino_@hotmail.com>
2023-05-16 00:44:07 -0300

    feat: implement process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+# from queue import Queue
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    my_txts = txt_importer(path_file)
+    outcome = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(my_txts),
+        "linhas_do_arquivo": my_txts,
+    }
+    for que_index in range(len(instance)):
+        if instance.search(que_index)["nome_do_arquivo"] == path_file:
+            return None
+
+    outcome_str = str(outcome)
+
+    instance.enqueue(outcome)
+    return sys.stdout.write(outcome_str)
 
 
 def remove(instance):
