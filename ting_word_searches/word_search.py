4325cec46f0e23dcfab7b95b6dbda6275537196f
Davi Albino <negroalbino_@hotmail.com>
2023-05-16 03:07:53 -0300

    feat: implement exists word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_words = list()
+
+    for index in range(len(instance)):
+        all_files = instance.search(index)
+        lines_with_word = list()
+
+# ref enumerate: https://www.datacamp.com/tutorial/python-enumerate-tutorial
+
+        for line_number, line in enumerate(all_files["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                lines_with_word.append({"linha": line_number + 1})
+
+        if lines_with_word:
+            result = {
+                "palavra": word,
+                "arquivo": all_files["nome_do_arquivo"],
+                "ocorrencias": lines_with_word
+            }
+            found_words.append(result)
+
+    return found_words
 
 
 def search_by_word(word, instance):
