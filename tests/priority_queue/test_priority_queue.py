564137ed0dded40a3c4b2b80b40c3b03bcc2f4b4
Davi Albino <negroalbino_@hotmail.com>
2023-05-16 02:06:44 -0300

    test: Priority Queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,20 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queues = PriorityQueue()
+    queues.enqueue({"qtd_linhas": 3})
+    queues.enqueue({"qtd_linhas": 4})
+    queues.enqueue({"qtd_linhas": 7})
+    queues.enqueue({"qtd_linhas": 6})
+    queues.enqueue({"qtd_linhas": 2})
+
+    assert queues.dequeue() == {"qtd_linhas": 3}
+    assert len(queues) == 4
+
+    queues.enqueue({"qtd_linhas": 1})
+    assert queues.search(2) == {"qtd_linhas": 1}
+
+    with pytest.raises(IndexError):
+        queues.search(75)
